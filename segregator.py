e9813e47e99626ba5739e0e71450c0ccda6f87cd
sriramcu <csriram12345@gmail.com>
2024-09-26 19:58:46 +0530

    Removed merge.py and outdated README

--- segregator.py
+++ segregator.py
@@ -1,49 +1,37 @@
-import os
-import sys
-import magic
-import re
-from PIL import Image, ExifTags
-import exiftool
-import shutil
-import datetime
-from pathlib import Path
-import platform
+import argparse
 import calendar
-import filecmp
+import datetime
+import mimetypes
+import os
+import platform
+import re
+import shutil
+from pathlib import Path, PurePath
+
+import exiftool
+from PIL import Image, ExifTags
+
+VERBOSE = False
+ASSUMPTIONS_FILE_NAME = "assumptions.txt"
+TIMESTAMP_STR = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 
 class ExtractionException(Exception):
     pass
 
 
-current_year = datetime.datetime.now().year
-years = list(range(1980, current_year))
-duplicate_count = 0
-
-
-def move_file(src, dst):
-    # Move without overwriting
-    src = os.path.abspath(src)
-    dst = os.path.abspath(dst)
-    if os.path.dirname(src) == os.path.dirname(dst):
-        # condition to ensure that file already present in the correct destination directory
-        # does not get renamed as a duplicate in the next condition
-        print("File already exists in the correct directory")
-        return
-    if os.path.exists(dst):
-        if filecmp.cmp(src, dst, shallow=False):
-            print("deleting identical file..")
-            os.remove(src)
-            global duplicate_count
-            duplicate_count += 1
-            return
-        dst_basename = "duplicate_" + os.path.basename(dst)
-        dst_dirname = os.path.dirname(dst)
-        while os.path.exists(os.path.join(dst_dirname, dst_basename)):
-            dst_basename = "duplicate_" + dst_basename
-        dst = os.path.join(dst_dirname, dst_basename)
-
-    shutil.move(src, dst)
+def segregate_based_on_creation_date(fpath, output_dir):
+    try:
+        year = datetime.datetime.fromtimestamp(creation_date(fpath)).strftime('%Y')
+        month = datetime.datetime.fromtimestamp(creation_date(fpath)).strftime('%b')
+    except Exception:
+        return False
+    else:
+        print_and_log(f"Year and month for {fpath} have been guessed as {year}, {month}", output_dir)
+        new_path = os.path.join(output_dir, year, month)
+        Path(new_path).mkdir(parents=True, exist_ok=True)
+        shutil.move(fpath, os.path.join(new_path, os.path.basename(fpath)))
+        return True
 
 
 def creation_date(path_to_file):
@@ -60,9 +48,10 @@ def creation_date(path_to_file):
 
 
 def is_video(fpath):
-    mime = magic.Magic(mime=True)
-    filename = mime.from_file(fpath)
-    if filename.find('video') != -1:
+    mt = mimetypes.guess_type(fpath)
+    if not mt:
+        return 0
+    if "video" in mt[0]:
         return 1
     return 0
 
@@ -86,124 +75,190 @@ def get_exif_date_time(fpath):
         raise ExtractionException("Error in extracting date time from file: " + str(e))
 
 
-def segregate_based_on_exif(fpath):
+def segregate_based_on_exif(fpath, output_dir):
     try:
         (month, year) = get_exif_date_time(fpath)
-        new_path = os.path.join('.', year, month)
+        new_path = os.path.join(output_dir, year, month)
         Path(new_path).mkdir(parents=True, exist_ok=True)
-        move_file(fpath, os.path.join(new_path, os.path.basename(fpath)))
-
+        shutil.move(fpath, os.path.join(new_path, os.path.basename(fpath)))
+        return True
     except ExtractionException:
-        # pass/send exception to caller
-        raise ExtractionException
+        return False
 
 
-def segregate_based_on_file_name(fpath):
+def segregate_based_on_file_name(fpath, output_dir):
     basename = str(os.path.basename(fpath))
     dirname = str(os.path.dirname(fpath))
 
+    current_year = datetime.datetime.now().year
+    years = list(range(1980, current_year))
+
     for year in years:
-        if str(year) in dirname:
+        if str(year) in PurePath(dirname).parts:
             # Already Segregated
-            return 1
+            return True
 
         if str(year) in basename:
             date_regex = re.compile(str(year) + r'[-_]?(\d{1,2})')
             # Year and month may be separated by hyphen or underscore
             mo = date_regex.search(basename)
             if not mo or int(mo.group(1)) > 12 or int(mo.group(1)) < 1:
-                exif_flag = 0
+                # Month not found in the basename of the file. Now try using EXIF metadata,
+                # since we already have year in the file name. In segregate by exif function, we only look at exif
+                # metadata without any regard to file or folder name.
+                exif_flag = False
                 try:
                     exif_month, exif_year = get_exif_date_time(fpath)
+                    # even though we have the year already, we are still going to validate the exif year with the filename
+                    # year. If the exif year is not equal to the filename year then we are not going to move the file
                 except ExtractionException:
                     exif_month = None
                     exif_year = None
-                    exif_flag = 1
+                    exif_flag = True
 
                 if not exif_flag and int(exif_year) == year:
+                    # No extraction error and exif year and filename year are same. Proceed to use exif month too.
                     month = exif_month
-                    print("Year and Month for {} have been guessed as {}-{}".format(fpath, year, month))
-                    new_path = os.path.join('.', str(year), month)
+                    print_and_log(f"Year and Month for {fpath} have been guessed as {year}-{month}",
+                                  output_dir)
+                    new_path = os.path.join(output_dir, str(year), month)
                     Path(new_path).mkdir(parents=True, exist_ok=True)
-                    move_file(fpath, os.path.join(new_path, basename))
-                    return 1
+                    shutil.move(fpath, os.path.join(new_path, basename))
+                    return True
 
-                print("Year for {} has been guessed as {}".format(fpath, year))
-                new_path = os.path.join('.', str(year))
+                print_and_log(f"Year for {fpath} has been guessed as {year}", output_dir)
+                new_path = os.path.join(output_dir, str(year))
                 Path(new_path).mkdir(parents=True, exist_ok=True)
-                move_file(fpath, os.path.join(new_path, basename))
-                return 1
+                shutil.move(fpath, os.path.join(new_path, basename))
+                return True
 
             else:
+                # No need for exif. Use year and month embedded in filename
                 month = str(mo.group(1))
                 month = calendar.month_abbr[int(month)]
-                print("Year and Month for {} have been guessed as {}-{}".format(fpath, year, month))
-                new_path = os.path.join('.', str(year), month)
+                print_and_log(f"Year and Month for {fpath} have been guessed as {year}-{month}", output_dir)
+                new_path = os.path.join(output_dir, str(year), month)
                 Path(new_path).mkdir(parents=True, exist_ok=True)
-                move_file(fpath, os.path.join(new_path, basename))
-                return 1
+                shutil.move(fpath, os.path.join(new_path, basename))
+                return True
 
-    return 0
-
-
-def segregate_based_on_creation_date(fpath):
-    try:
-        year = datetime.datetime.fromtimestamp(creation_date(fpath)).strftime('%Y')
-        month = datetime.datetime.fromtimestamp(creation_date(fpath)).strftime('%b')
-    except Exception:
-        raise ExtractionException
-        # return 0
-    print("Year and month for {} have been guessed as {},{}".format(fpath, year, month))
-    new_path = os.path.join('.', year, month)
-    Path(new_path).mkdir(parents=True, exist_ok=True)
-    move_file(fpath, os.path.join(new_path, os.path.basename(fpath)))
-    return 1
+    return False
 
 
-def main():
-    if len(sys.argv) < 2:
-        print("Usage: python3 segregator.py parent_dir")
-        sys.exit(-1)
+def print_and_log(msg, output_dir, first_time=False, verbose=VERBOSE):
+    if first_time:
+        mode = 'w'
+    else:
+        mode = 'a'
+    if verbose:
+        print(msg)
 
-    os.chdir(sys.argv[1])
-    # Argument must specify parent directory containing the files to be segregated; this program preserves all directories
-    assumptions_file_name = "assumptions.txt"
-    assumptions_file_pointer = open(assumptions_file_name, 'w')  # Stores names of files whose date were guessed
-
-    num_files = sum([len(files) for r, d, files in os.walk(".")]) - 1
-    i = 0
-    files_list = []
-    for (root, dirs, files) in os.walk('.', topdown=True):
-        for img_file_path in files:
-            files_list.append(os.path.abspath(os.path.join(root, img_file_path)))
-
-    for img_file_path in files_list:
-        if i:
-            print("{} out of {} files have been segregated!".format(i, num_files))
-        i += 1
-        if assumptions_file_name in img_file_path:
-            continue
-
-        flag = segregate_based_on_file_name(img_file_path)
-        if flag:
-            continue
-
-        try:
-            segregate_based_on_exif(img_file_path)
-        except ExtractionException:
-            print("Not able to get actual timestamp of {}, segregating based on creation date...".format(img_file_path))
-            assumptions_file_pointer.write(img_file_path + '\n')
-            try:
-                segregate_based_on_creation_date(img_file_path)
-            except ExtractionException:
-                print("Year for {} could not be guessed".format(img_file_path))
-                move_file(img_file_path, os.path.join(os.path.abspath(sys.argv[1]), os.path.basename(img_file_path)))
-
-    # print("{} out of {} files have been segregated!".format(i, num_files))
-    print("{} identical files deleted".format(duplicate_count))
+    assumptions_file_path = os.path.join(output_dir, ASSUMPTIONS_FILE_NAME)
+    assumptions_file_pointer = open(assumptions_file_path, mode)  # Stores names of files whose date were guessed
+    assumptions_file_pointer.write(msg + '\n')
     assumptions_file_pointer.close()
 
 
+def get_directory_stats(path):
+    total_size = 0
+    file_count = 0
+    if os.path.exists(path):
+        for dirpath, dirnames, filenames in os.walk(path):
+            for filename in filenames:
+                filepath = os.path.join(dirpath, filename)
+                # Skip if it's a broken symlink
+                if os.path.exists(filepath):
+                    total_size += os.path.getsize(filepath)
+                    file_count += 1
+
+    return file_count, total_size
+
+
+def backup_input_or_output_folder(directory, output_dir):
+    """
+    Creates a backup of the input or output folder.The backup folder will be a sibling to directory and the actual
+    backup is stored inside the appropriate timestamp folder.
+    :param directory:
+    :return:
+    """
+    print_and_log(f"Backing up {directory}", output_dir)
+
+    backup_dir = os.path.join(os.path.dirname(directory), 'program_backup', TIMESTAMP_STR, os.path.basename(directory))
+    os.makedirs(backup_dir, exist_ok=True)
+    for root, dirs, files in os.walk(directory):
+        for directory in dirs:
+            directory_path = os.path.join(root, directory)
+            os.makedirs(os.path.join(backup_dir, os.path.relpath(directory_path, directory)), exist_ok=True)
+        for file in files:
+            shutil.copy2(os.path.join(root, file), os.path.join(backup_dir, os.path.relpath(root, directory), file))
+
+    print_and_log(f"Backup of {directory} completed", output_dir)
+    # cleanup old backups
+    backup_parent_dir = os.path.join(os.path.dirname(directory), 'program_backup')
+    if not os.path.exists(backup_parent_dir):
+        return
+    backups = [os.path.join(backup_parent_dir, f) for f in sorted(os.listdir(backup_parent_dir), reverse=True)]
+    for backup in backups[4:]:
+        shutil.rmtree(backup)
+
+
+def segregate_entire_folder(input_dir, output_dir):
+    if not os.path.exists(input_dir):
+        raise FileNotFoundError(f"Input directory {input_dir} does not exist")
+    os.makedirs(output_dir, exist_ok=True)
+    print_and_log(f"File count and file size of input folder {input_dir} is {get_directory_stats(input_dir)}",
+                  output_dir, verbose=True, first_time=True)
+    print_and_log(f"Before program run, file count and file size of output folder {output_dir}"
+                  f" is {get_directory_stats(output_dir)}", output_dir, verbose=True)
+    backup_input_or_output_folder(input_dir, output_dir)
+    if (get_directory_stats(output_dir)[0] > 1):
+        backup_input_or_output_folder(output_dir, output_dir)
+    for (root, dirs, files) in os.walk(input_dir, topdown=True):
+        for img_file_name in files:
+            if ASSUMPTIONS_FILE_NAME in img_file_name:
+                continue
+            img_file_path = os.path.abspath(os.path.join(root, img_file_name))
+
+            status_flag = segregate_based_on_file_name(img_file_path, output_dir)
+            if status_flag:
+                continue
+            print_and_log(f"Unable to segregate {img_file_path} based on its file name.", output_dir)
+
+            status_flag = segregate_based_on_exif(img_file_path, output_dir)
+            if status_flag:
+                continue
+            print_and_log(f"Unable to segregate {img_file_path} based on its EXIF metadata.", output_dir)
+
+            status_flag = segregate_based_on_creation_date(img_file_path, output_dir)
+            if status_flag:
+                continue
+            print_and_log(f"Unable to segregate {img_file_path} based on its creation date.", output_dir)
+
+            unknown_dir_path = os.path.join(os.path.abspath(output_dir), "Unknown")
+            os.makedirs(unknown_dir_path, exist_ok=True)
+            shutil.move(img_file_path, os.path.join(unknown_dir_path, img_file_name))
+
+    print_and_log(f"After program run, file count and file size of output folder {output_dir}"
+                  f" is {get_directory_stats(output_dir)}", output_dir, verbose=True)
+    if len(os.listdir(input_dir)) == 0:
+        shutil.rmtree(input_dir)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Segregate files into folders based on their date')
+    parser.add_argument('-d', '--input_dir', type=str, required=True,
+                        help='Absolute path of folder containing the files to be segregated')
+    parser.add_argument('-o', '--output_dir', type=str, required=True,
+                        help='Absolute path of output directory')
+    parser.add_argument('-v', '--verbose', type=int, default=0,
+                        help='Print status messages as the program runs')
+    args = parser.parse_args()
+    input_dir = args.input_dir
+    output_dir = args.output_dir
+    global VERBOSE
+    VERBOSE = bool(int(args.verbose))
+    segregate_entire_folder(input_dir, output_dir)
+
+
 if __name__ == '__main__':
-    # todo merge program and GUI
     main()
